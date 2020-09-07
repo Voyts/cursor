9d7082f801d562e3da57169f382d84240eba2a8c
Voyts <alexandervoyts@gmail.com>
2020-09-07 11:01:34 +0300

    alpha version homework

---
+++ 09-dom/script.js
@@ -0,0 +1,16 @@
+const square = document.body.insertAdjacentHTML('beforeend', '<div id="cont">' + '<div></div>'.repeat(25) + '</div>');
+const blocks = document.querySelectorAll('#cont>div');
+
+function generateBlocks() {
+    blocks.forEach(el => {
+        el.style.backgroundColor = '#' + Math.random().toString(16).slice(2, 8);
+        el.onclick = e => generateBlocks();
+    });
+
+}
+
+function generateBlocksInterval() {
+    setInterval(generateBlocks, 1000);
+}
+
+generateBlocks();
